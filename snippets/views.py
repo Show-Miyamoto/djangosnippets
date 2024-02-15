ca39b077e22c1e8f6264713c5a6a48d4fa921bb1
sho <sho@sho-no-MacBook-Pro.local>
2024-02-15 12:36:37 +0900

    initial commit

---
+++ snippets/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from snippets.models import Snippet  # Snippetモデルをインポート
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+
+from snippets.forms import SnippetForm
+
+
+def top(request):
+    snippets = Snippet.objects.all()
+    context = {"snippets": snippets}
+    return render(request, "snippets/top.html", context)
+
+@login_required
+def snippet_new(request):
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            snippet = form.save(commit=False)
+            snippet.created_by = request.user
+            snippet.save()
+            return redirect(snippet_detail, snippet_id=snippet.pk)
+        else:
+            form = SnippetForm()
+        return render(request, "snippets/snippet_new.html", {'form': form})
+
+
+def snippet_edit(request, snippet_id):
+    return HttpResponse('スニペットの編集')
+
+
+def snippet_detail(request, snippet_id):
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    return render(request, 'snippets/snippet_detail.html', {'snippet': snippet})
+    # return HttpResponse('スニペットの詳細閲覧')
